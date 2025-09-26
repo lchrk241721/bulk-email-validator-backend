f007372f44457ff9f83b3f0245cf0d099cf2daaf
Chaitanya Rajkumar Limmala <lchrk2417@aol.com>
2025-09-26 19:11:28 +0530

    Fixed "Make.com" integration

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -31,6 +31,6 @@ router.post('/upload-csv', upload.single('file'), emailController.parseCSV);
 
 // Make.com integration routes
 router.post('/make/integration', (req, res) => emailController.makeIntegration(req, res));
-router.get('/make/webhook', (req, res) => emailController.makeWebhookTest(req, res));
+router.post('/make/webhook', (req, res) => emailController.makeWebhookTest(req, res));
 
 module.exports = router;
